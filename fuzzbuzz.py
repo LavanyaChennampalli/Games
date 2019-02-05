50a8ece2bbaf335c0e97f05ceb6dd2f055ac4521
LavanyaChennampalli <46275272+LavanyaChennampalli@users.noreply.github.com>
2019-02-05 18:42:19 +0530

    Add files via upload

---
+++ fuzzbuzz.py
@@ -0,0 +1,15 @@
+for i in range(51):
+    if(i%3==0 and i%5==0):
+        print(i,"fuzzbuzz")
+    elif(i%3==0):
+        print(i,"fuzz")
+    elif(i%5==0):
+        print(i,"buzz")
+    
+    else:
+        print(i)
+
+
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                           
